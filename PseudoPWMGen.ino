b04596b5ee9c788d75ca81dd93e7820b93ed3734
Yuu528 <yuu528mail@gmail.com>
2020-12-12 08:27:31 +0900

    Improved stability & Removed test code

--- PseudoPWMGen.ino
+++ PseudoPWMGen.ino
@@ -1,44 +1,29 @@
 #include <TimerOne.h>
 
-//PWM周波数[Hz] FPWM_RES = 256なら200Hzくらいが限界
-#define FPWM_FREQ 490
+//PWM周波数[Hz]
+#define FPWM_FREQ 50
 
 //PWM分解能 小さくすると周波数上限を上げられる
-#define FPWM_RES  512
+#define FPWM_RES 16
 
 //ピンの出力値用 0pin(RX)と1pin(TX)は使えない
 volatile unsigned int fAnalogWritePins[18] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 unsigned int fAnalogWriteCount = 0; //PWM周期カウンタ
 
 void setup() {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-
   //PWM分解能と周波数より割り込み周期(出力1のときのON時間[us])を計算、割り込みを設定
   Timer1.initialize(1000000 / FPWM_RES / FPWM_FREQ);
   Timer1.attachInterrupt(fAnalogInterrupt);
 }
 
 void loop() {
-  //テスト用プログラム
-  //Pin2に偽PWM出力0%~100%を周期的に出す
-  //比較用としてPin3にAVRのPWM出力をする
-  for(int i = 0; i <= 512; i++) {
-    fAnalogWrite(2, i);
-    analogWrite(3, i);
-    delay(1);
-  }
-  for(int i = 512; i >= 0; i--) {
-    fAnalogWrite(2, i);
-    analogWrite(3, i);
-    delay(1);
-  }
+  //なにもしない
 }
 
 /*
  * fAnalogWrite 指定したPINから偽のPWM出力をする
  * (byte) pin   : ピン番号(2 <= pin <= 19)
- * (byte) value : PWM出力値(0 <= pin <= FPWM_RES)
+ * (byte) value : PWM出力値(0 <= value <= FPWM_RES)
  */
 void fAnalogWrite(byte pin, unsigned int value) {
   //0, 1pinを省くためpin - 2とする
@@ -54,9 +39,9 @@ void fAnalogInterrupt() {
     for(byte i = 0; i <= 5; i++) {
       //fAnalogWritePins[pin]に設定されたPWM比を満たしたらLOWに
       //+2や+6、+12をしてforカウンタと配列添字、ポート番号をうまく揃える
-      if(fAnalogWritePins[i] == fAnalogWriteCount) PORTD &= ~_BV(i + 2);
-      if(fAnalogWritePins[i + 6] == fAnalogWriteCount) PORTB &= ~_BV(i);
-      if(fAnalogWritePins[i + 12] == fAnalogWriteCount) PORTC &= ~_BV(i);
+      if(fAnalogWritePins[i] <= fAnalogWriteCount) PORTD &= ~_BV(i + 2);
+      if(fAnalogWritePins[i + 6] <= fAnalogWriteCount) PORTB &= ~_BV(i);
+      if(fAnalogWritePins[i + 12] <= fAnalogWriteCount) PORTC &= ~_BV(i);
     }
     fAnalogWriteCount++;
     //PWM100%地点に達したらまたはじめから
